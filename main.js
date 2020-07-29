b9db91b38e1ef33e7cd65bfd3c4e4c575b8fdc74
Francis Eclevia <65243967+francisecleviaCI@users.noreply.github.com>
2020-07-28 22:39:28 -0400

    hw

--- main.js
+++ main.js
@@ -2,6 +2,85 @@
  * YOUR CODE HERE *
  ******************/
 
+const newPerson = function(newFirstName, newLastName, newAge, isMarried){
+  // return [];
+
+if (newFirstName === undefined){
+  newFirstName = 'Anonymous'
+}
+
+if (newLastName === undefined){
+  newLastName = 'Person'
+}
+
+if (isMarried !== undefined){
+  isMarried = false
+}
+
+  const theNewPerson = {
+    firstName: newFirstName,
+    lastName: newLastName,
+    age: newAge,
+    married: isMarried,
+    goingOn: function(){
+      return newAge + 1;
+    },
+    ageUp: function(){
+      return newAge + 1;
+    },
+    getFullName: function(){
+      return newFirstName + ' ' + newLastName
+    }
+
+
+  }
+
+  // const person2 = {
+  //   firstName: newFirstName,
+  //   lastName: lastName,
+  //   age: newAge,
+  //   isMarried: true,
+
+    
+  // }
+
+
+
+const person1 = newPerson(newFirstName, newLastName, newAge, isMarried )
+
+person1.firstName;
+
+person1.goingOn()
+
+}
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*********************************
